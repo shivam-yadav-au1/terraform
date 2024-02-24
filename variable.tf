c8ffc72315eaba0c1a1a83677d43e7bcbe3e2bd3
shivam-appinventiv <shivam.yadav1@appinventiv.com>
2024-02-24 14:13:54 +0530

    initial commit

---
+++ variable.tf
@@ -0,0 +1,71 @@
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["172.27.112.0/21", "172.27.120.0/22", "172.27.124.0/22"]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+  default     = ["172.27.64.0/20", "172.27.80.0/20", "172.27.96.0/20"]
+}
+
+variable "vpc-cidr-range" {
+  type = string
+  description = "vpc-cidr"
+  default = "172.27.64.0/18"
+}
+
+variable "availability-zones" {
+  type        = list(string)
+  description = "Availablity zones"
+  default     = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
+}
+
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+
+variable "ami_id_launch_template" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = string
+}
+
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "redis_cluster_name"{
+  type = string
+}
+
+variable "environment" {
+  type = string
+  
+}
+
+variable "auth_token" {
+  type = string
+  default = ""
+}
+
+variable "vpc_name"{
+  type = string
+  default = "smoke-uat"
+}
+
+variable "ecs_ec2_server"{
+  type = string
+  default = "ECS-ec2-server"
+}
---
+++ vpc.tf
@@ -0,0 +1,100 @@
+resource "aws_eip" "elastic-ip-tf" {
+  tags = {
+    Name = "${var.vpc_name}-elastic-ip"
+  }
+}
+
+resource "aws_internet_gateway" "internet-gw" {
+  vpc_id = aws_vpc.vpc-tf.id
+
+  tags = {
+    Name = "${var.vpc_name}-internet-gateway"
+  }
+}
+
+resource "aws_subnet" "vpc-tf-public-subnets" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.vpc-tf.id
+  cidr_block              = element(var.public_subnet_cidrs, count.index)
+  availability_zone       = element(var.availability-zones, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.vpc_name}-public-subnet-${count.index + 1}"
+  }
+}
+
+
+resource "aws_subnet" "vpc-tf-private-subnets" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.vpc-tf.id
+  cidr_block = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.availability-zones,count.index)
+
+  tags = {
+    Name = "${var.vpc_name}-private-subnet-${count.index + 1}"
+  }
+}
+
+ resource "aws_nat_gateway" "nat-gateway-tf" {
+  allocation_id = aws_eip.elastic-ip-tf.id
+  subnet_id     = aws_subnet.vpc-tf-public-subnets[0].id
+
+  tags = {
+    Name = "${var.vpc_name}-NATGATEWAY"
+  }
+  depends_on = [aws_internet_gateway.internet-gw]
+} 
+
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.vpc-tf.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-public-route-table"
+  }
+}
+
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.vpc-tf.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gateway-tf.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-private-route-table"
+  }
+}
+
+
+
+resource "aws_route_table_association" "route-table-association-public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.vpc-tf-public-subnets[*].id, count.index)
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "route-table-association-private" {
+  count =  length(var.private_subnet_cidrs)
+  subnet_id      = element(aws_subnet.vpc-tf-private-subnets[*].id,count.index)
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+
+resource "aws_vpc" "vpc-tf" {
+  cidr_block = "${var.vpc-cidr-range}"
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+
+
